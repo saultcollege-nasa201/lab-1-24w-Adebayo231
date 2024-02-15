190bd8660ed60f8f12493607a4dd8293bd0cb502
Adebayo231 <157163356+Adebayo231@users.noreply.github.com>
2024-02-15 03:06:06 +0000

    Updated

--- docs/myjs.js
+++ docs/myjs.js
@@ -1,7 +1,37 @@
-console.log("hi from the script file");
+function verifyform() {
+    var Message = document.forms["mforms"]["Message"].value;
+    if (Message == "")
+    {
+    alert("Input Message");
+    return false;
+    }
 
-function add(a, b){
-    let i= a + b
-    console.log (i);
+    var CC = document.forms["mforms"]["CC"].value;
+    if (CC == "")
+    {
+    alert("Input CC");
+    return false;
+    }
+
+    var lname = document.forms["mforms"]["lname"].value;
+    if (lname == "")
+    {
+    alert("Input Last Name");
+    return false;
+    }
+
+    var radioButtons = document.getElementsByName("fav_language");
+    var isChecked = false;
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            isChecked = true;
+            break;
+        }
+    }
+    if (!isChecked) {
+        alert("Please confirm if you want to send without header");
+        return false;
+    }
 }
+
 
